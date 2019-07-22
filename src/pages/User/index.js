6b2ea7e181eafba3d25a9d1a4e9836197c512568
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-22 12:50:26 -0300

    update: listing stars of the GitHub

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Container, Text } from './styles';
 import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -10,7 +23,7 @@ export default class User extends Component {
 
   static propTypes = {
     navigation: PropTypes.shape({
-      navigate: PropTypes.func,
+      getParam: PropTypes.func,
     }).isRequired,
   };
 
@@ -22,17 +35,38 @@ export default class User extends Component {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const reponse = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: Response.data });
+    this.setState({ stars: response.data });
   }
 
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
+
+    const user = navigation.getParam('user');
 
     return (
       <Container>
-        <Text>Page User</Text>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
       </Container>
     );
   }
