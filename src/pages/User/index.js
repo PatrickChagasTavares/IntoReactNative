af17e2f70e80047d28a580d50efaafba457349d0
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-22 11:51:26 -0300

    create: method to search data in gitHub and add in page User

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Container, Text } from './styles';
+import api from '../../services/api';
 
-export default function User(props) {
-  console.tron.log(props);
-  return (
-    <Container>
-      <Text>Page User</Text>
-    </Container>
-  );
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const reponse = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: Response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+
+    return (
+      <Container>
+        <Text>Page User</Text>
+      </Container>
+    );
+  }
 }
